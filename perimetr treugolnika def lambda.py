b6dc3a54fe6db4b43bad719f80e7a9e2ab6eeef7
Aleksandr Zelukin <alexandr44@inbox.lv>
2022-04-04 16:33:25 +0300

    Apps update

---
+++ perimetr treugolnika def lambda.py
@@ -0,0 +1,37 @@
+a=int(input())
+b=int(input())
+c=int(input())
+
+if a+b>c and a+c>b and b+c>a:
+    p=a+b+c
+    print(p)
+
+else:
+    print("trijsturis neekziste")
+
+
+
+def tr (a,b,c):
+
+    if a+b>c and a+c>b and b+c>a:
+        return a+b+c
+    else:
+        print("trijsturis neeksiste")
+    
+
+a=int(input())
+b=int(input())
+x=int(input())
+print(tr(a,b,x))
+
+
+
+
+d = lambda a,b,c:a+b+c
+    
+print("trijsturis neeksiste")
+
+
+
+
+print(d(5,7,7))     
