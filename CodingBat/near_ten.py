7c5cf95b79ade3daa0be3eecb6fd0bcf4b19f9cf
AleksandrZelukin <92846541+AleksandrZelukin@users.noreply.github.com>
2022-04-14 09:13:07 +0300

    Apps update

---
+++ CodingBat/near_ten.py
@@ -0,0 +1,13 @@
+def near_ten(num):
+    rem = num % 10
+    if rem <= 2 and rem >= 0:
+        return True
+    elif rem >= 8 and rem <= 9:
+        return True
+    else:
+        return False
+        
+        
+a=17
+
+print(near_ten(a))
