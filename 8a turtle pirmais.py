fa1af6cef2c78eb21fd135480b043ff9754ce66c
AleksandrZelukin <92846541+AleksandrZelukin@users.noreply.github.com>
2022-05-12 13:33:49 +0300

    update datnes

--- 8a turtle pirmais.py
+++ 8a turtle pirmais.py
@@ -1,20 +1,29 @@
 from turtle import *
 
-col=["red","blue","yellow","darkred"]
-shape("turtle")
+color('yellow')
+up()
+goto(-350,250)
+down()
+pensize(6)
+begin_fill()
+for i in range (12):
+    fd(200)
+    bk(200)
+    lt(30)
+    i+=1
 
-def f (x,y,a,s,z,q):
-    penup()
-    goto(x,y)
-    color(col[q])
-    pensize(z)
-    pendown()
-    for i in range (s):
-        fd(a)
-        lt(360/s)
-        i += 1
-        
-    
-f(-100,-200,30,4,6,0)
-f(100,200,40,8,4,3)
-f(0,0,4,96,2,2)
+goto(-350, 150)
+circle(100)
+end_fill()
+
+
+
+
+
+
+
+
+
+
+
+done()
