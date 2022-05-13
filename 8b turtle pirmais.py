bb1d98065df26a887793c84be0e68f27eb428da8
AleksandrZelukin <92846541+AleksandrZelukin@users.noreply.github.com>
2022-05-13 13:06:41 +0300

    Update 8b turtle pirmais.py

--- 8b turtle pirmais.py
+++ 8b turtle pirmais.py
@@ -1,5 +1,20 @@
 from turtle import *
 
+color('yellow')
+up()
+goto(-350,250)
+down()
+pensize(6)
+begin_fill()
+for i in range (12):
+    fd(200)
+    bk(200)
+    lt(30)
+    i+=1
+
+goto(-350, 150)
+circle(100)
+end_fill()
 
 color("#AA00FF")
 penup()
