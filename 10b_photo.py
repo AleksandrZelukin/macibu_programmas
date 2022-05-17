97454dd0b7cc7583e27b0a6c28ca632fc28ae0b4
AleksandrZelukin <92846541+AleksandrZelukin@users.noreply.github.com>
2022-05-17 09:34:22 +0300

    Update 10b_photo.py

--- 10b_photo.py
+++ 10b_photo.py
@@ -3,8 +3,8 @@ from tkinter import *
 logs = Tk()
 logs.title("Rīgas 92.vidusskolas emblēma")
 logs.geometry("600x600")
-textLabel = Label(logs, text="Rīgas 92.vidusskola",justify=CENTER,padx=10)
-textLabel.pack(side=BOTTOM)
+textLabel = Label(logs, text="Rīgas 92.vidusskola",justify=RIGHT,padx=10)
+textLabel.pack(side=LEFT)
 
 photo = PhotoImage(file="92 vsk logo.png")
 imgLabel = Label(logs, image=photo)
