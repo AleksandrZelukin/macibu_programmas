fdcbc7a0acc609d68c7600afd2f9aa5d5b06ac97
Aleksandr <alexandr44@inbox.lv>
2022-09-15 20:10:48 +0300

    Update abstract_class.py

--- OOP/abstract_class.py
+++ OOP/abstract_class.py
@@ -1,26 +1,43 @@
 # abstract class - a class which contains one or more abstract methods.
 # abstarct methods = a method that has a declaration but does not have an implementation.
 
+from abc import ABC, abstractmethod
 
+class Vehicle(ABC):
 
-class Vehicle:
+    @abstractmethod
     def go (self):
         pass
+
+    @abstractmethod
+    def stop(self):
+        pass
+
 class Car(Vehicle):
 
     def go (self):
         print("Your drive the car")
+
+    def stop(self):
+        print("This car is stopped")
+        
 
 class Motorcycle(Vehicle):
 
     def go (self):
         print("You ride on motorcycle")
 
-vehicle = Vehicle()
+    def stop(self):
+        print("This motrocycle is stopped")
+
+#vehicle = Vehicle()
 car = Car()
 motorcycle = Motorcycle()
 
-vehicle.go()
+#vehicle.go()
 car.go()
 motorcycle.go()
+
+#car.stop()
+motorcycle.stop()
 
