e23186d836a09422fe1573764f6a18bfa4908f50
Aleksandr <alexandr44@inbox.lv>
2022-10-06 19:51:50 +0300

    update dialogs

--- PySimpleGUI_programmas/dialogs.py
+++ PySimpleGUI_programmas/dialogs.py
@@ -1,22 +1,24 @@
 import PySimpleGUI as sg
 
 # Define the window's contents
-layout = [[sg.Text("Kā tevi sauc?")],
-          [sg.Input(key='-INPUT-')],
+class logs:
+    layout = [[sg.Text("Kartes numurs")],
+          [sg.Input('1234567899', key='-INPUT-')],
           [sg.Text(size=(40,1), key='-OUTPUT-')],
-          [sg.Button('Ok'), sg.Button('Quit')]]
-
+          [sg.Button('Ok'), sg.Button('Quit')],
+          [sg.Button('Parbaudit')]]
 # Create the window
-window = sg.Window('Jautājums', layout)
+    window = sg.Window('Jautājums', layout)
 
 # Display and interact with the Window using an Event Loop
-while True:
-    event, values = window.read()
+    while True:
+        event, values = window.read()
     # See if user wants to quit or window was closed
-    if event == sg.WINDOW_CLOSED or event == 'Quit':
-        break
+        if event == sg.WINDOW_CLOSED or event == 'Quit':
+            break
     # Output a message to the window
-    window['-OUTPUT-'].update('Hello ' + values['-INPUT-'] + "! Thanks for trying PySimpleGUI")
-    print(values['-INPUT-'])
+    # window['-OUTPUT-'].update(values['-INPUT-'])
+        print(values['-INPUT-'])
+
 # Finish up by removing from the screen
-window.close()                            # Part 5 - Close the Window
+    window.close()
