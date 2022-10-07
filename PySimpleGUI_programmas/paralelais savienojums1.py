a15ddd993fe7b0811ad9fa37536aac36b6550a9b
Aleksandr <alexandr44@inbox.lv>
2022-10-07 20:16:54 +0300

    update parallels

--- PySimpleGUI_programmas/paralelais savienojums1.py
+++ PySimpleGUI_programmas/paralelais savienojums1.py
@@ -1,42 +1,30 @@
 import PySimpleGUI as sg
-#print("aprēķiniet rezistoru paralēlā savienojuma pretestību")
 
-#print("Cik rezistoru ir ķēdē? max=5")
-'''count=int(input())
-if count == 2:
-    r1=float(input("R1="))
-    r2=float(input("R2="))
-    r=round(1/(1/r1+1/r2),2)
-    print("R= ",r,"Ohm")
+def kopa (r1, r2):
+    r1, r2, = float(input(r1)), float(input(r2))
+    r_par = round(1/(1/r1+1/r2),2)
+    return (f'Kopejas pretestiba: {r_par}')
 
-if count == 3:
-    r1=float(input("R1="))
-    r2=float(input("R2="))
-    r3=float(input("R3="))
-    r=round(1/(1/r1+1/r2+1/r3),2)
-    print("R= ",r,"Ohm")
+layout = [[sg.Text("Paralelais savienojums")],
+          [sg.Text("R1"),sg.Input(key='R1')],
+          [sg.Text("R2"),sg.Input(key='R2')],
+          
+          [sg.Text(size=(40,1), key='-OUTPUT-')],
+            [sg.Button('cik?', key='submit')],
+          [sg.Button('Ok'), sg.Button('Quit')]]
 
 
-if count == 4:
-    r1=float(input("R1="))
-    r2=float(input("R2="))
-    r3=float(input("R3="))
-    r4=float(input("R4="))
-    r=round(1/(1/r1+1/r2+1/r3+1/r4),2)
-    print("R= ",r,"Ohm")
+window = sg.Window('Jautājums', layout)
 
-if count == 5:
-    r1=float(input("R1="))
-    r2=float(input("R2="))
-    r3=float(input("R3="))
-    r4=float(input("R4="))
-    r5=float(input("R5="))
-    r=round(1/(1/r1+1/r2+1/r3+1/r4+1/r5),2)
-    print("R= ",r,"Ohm")
-'''
-layot = [[sg.Text('R1'), sg.InputText(size=(10,1))],
-        [sg.Text('aprēķiniet rezistoru paralēlā savienojuma pretestību')]]
 
-window = sg.Window('Calculator R+R', layot)
+while True:
+    event, values = window.read()
 
-window.Close
+    if event == 'submit':
+        r_par = kopa(values[0], values[1])
+        window['r_par'].update (r_par)
+        print(r_par)
+    if event == sg.WINDOW_CLOSED or event == 'Quit':
+        break
+
+window.close()
