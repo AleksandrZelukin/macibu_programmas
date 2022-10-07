e311832a8bfb6719c19a835225a5d37335d91f21
AleksandrZelukin <92846541+AleksandrZelukin@users.noreply.github.com>
2022-10-07 09:57:16 +0300

    Update parallels

--- PySimpleGUI_programmas/paralelais savienojums1.py
+++ PySimpleGUI_programmas/paralelais savienojums1.py
@@ -1,7 +1,8 @@
-print("aprēķiniet rezistoru paralēlā savienojuma pretestību")
+import PySimpleGUI as sg
+#print("aprēķiniet rezistoru paralēlā savienojuma pretestību")
 
-print("Cik rezistoru ir ķēdē? max=5")
-count=int(input())
+#print("Cik rezistoru ir ķēdē? max=5")
+'''count=int(input())
 if count == 2:
     r1=float(input("R1="))
     r2=float(input("R2="))
@@ -32,4 +33,10 @@ if count == 5:
     r5=float(input("R5="))
     r=round(1/(1/r1+1/r2+1/r3+1/r4+1/r5),2)
     print("R= ",r,"Ohm")
+'''
+layot = [[sg.Text('R1'), sg.InputText(size=(10,1))],
+        [sg.Text('aprēķiniet rezistoru paralēlā savienojuma pretestību')]]
 
+window = sg.Window('Calculator R+R', layot)
+
+window.Close
