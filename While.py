26f9adfaaaedcf1269f9d3b40d4ff76d2cf8ac97
AleksandrZelukin <92846541+AleksandrZelukin@users.noreply.github.com>
2022-10-17 15:49:01 +0300

    Create While.py

---
+++ While.py
@@ -0,0 +1,12 @@
+print("Cikla while izmatošanas piemers")
+
+while True:
+    y = input("Izvelēs saivenojumu vedids:\nparalelais nospiež 1, virknes - 2\n")
+    if y == "1":
+        print("Paralelais savienojums")
+    if y == "2":
+        print("virknes savienojums")
+        
+    if y == "0":
+        break
+        
