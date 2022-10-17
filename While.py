0bcb1aea33654a5624d4824dba49dbc95474596f
Aleksandr <alexandr44@inbox.lv>
2022-10-17 21:27:46 +0300

    Update while.py

--- While.py
+++ While.py
@@ -1,12 +1,13 @@
 print("Cikla while izmatošanas piemers")
-
+print("Izvelēs saivenojumu vedids:\nparalelais nospiež 1, virknes - 2\n")
 while True:
-    y = input("Izvelēs saivenojumu vedids:\nparalelais nospiež 1, virknes - 2\n")
-    if y == "1":
+    y = int(input())
+    if y == 1:
         print("Paralelais savienojums")
-    if y == "2":
+    if y == 2:
         print("virknes savienojums")
         
-    if y == "0":
+    if y >= 3:
+        print("nepareizi dati!")
         break
         
