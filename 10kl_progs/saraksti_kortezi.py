ab8d46af0d5868ad9c8a71cdd0013f6f4a3de662
AleksandrZelukin <92846541+AleksandrZelukin@users.noreply.github.com>
2022-10-31 15:41:44 +0200

    Update programms

---
+++ 10kl_progs/saraksti_kortezi.py
@@ -0,0 +1,7 @@
+a = [12, 34, 56, 67, 15]
+a = tuple(a)
+a = list(a)
+
+
+
+print(a, type(a))
---
+++ 10kl_progs/vardnicas.py
@@ -0,0 +1,22 @@
+
+a = {}
+
+a[1]=100
+a[2]=200
+a[3]=300
+
+print(a)
+
+del a[2]
+
+print(a)
+
+d={}
+while True:
+    a= input()
+    b= input()
+    d[a]=b
+    print(d)
+    if a =="stop":
+        del d["stop"]
+        break
