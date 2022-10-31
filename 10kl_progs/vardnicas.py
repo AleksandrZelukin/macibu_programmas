aa99b462acc4328bc89d900b5e150dcf85c359d8
Aleksandr <alexandr44@inbox.lv>
2022-10-31 17:28:47 +0200

    update vardnicas.py

--- 10kl_progs/vardnicas.py
+++ 10kl_progs/vardnicas.py
@@ -1,4 +1,4 @@
-
+'''
 a = {}
 
 a[1]=100
@@ -10,14 +10,24 @@ print(a)
 del a[2]
 
 print(a)
-
+'''
 d={}
+print("Nospiediet stop, lai izietu!")
 while True:
-    a= input()
-    b= input()
+    a= input("Key: ")
+    if a =="stop":
+        b=a
+        d[a]=b
+        del d["stop"]
+        print(d)
+        break
+    else:
+        b= input("Value: ")
     d[a]=b
     print(d)
+    '''
     if a =="stop":
         del d["stop"]
         print(d)
         break
+    '''
