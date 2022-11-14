3ff81caaabf5f8efc6d6513cd329c605643f3e66
Aleksandr <alexandr44@inbox.lv>
2022-11-14 22:31:51 +0200

    Update programms

--- 10kl_progs/pirma_turtle.py
+++ 10kl_progs/pirma_turtle.py
@@ -10,16 +10,16 @@ a.begin_fill()
 for i in range(4):
     a.forward(100)
     a.right(90)
-    i +=1
+    
 a.end_fill()
 a.up()
 a.goto(-200,100)
 a.down()
 a.fillcolor("blue")
 a.begin_fill()
-for i in range(3):
+for i in range(5):
     a.forward(100)
-    a.left(120)
+    a.left(72)
 a.end_fill() 
 
 a.screen.mainloop()
