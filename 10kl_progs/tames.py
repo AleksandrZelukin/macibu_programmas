3ba5d3b46c660bff1a980852f87a533b5362dbc1
Aleksandr Zelukin <alexandr44@inbox.lv>
2022-11-22 10:14:57 +0200

    update tames

--- 10kl_progs/tames.py
+++ 10kl_progs/tames.py
@@ -8,6 +8,7 @@ kas no dotiem argumentiem aprēķina un atgriež Andrim derīgu rezultātu.
 '''
 
 import math
+'''
 print("Cenas")
 l = float(input("Lamināts(par paku),euro: "))
 t = float(input("Tapetes(par rulli),euro: "))
@@ -28,9 +29,9 @@ z = float(input("platums:"))
 
 tame = list(remonts(x,y,z))
 
-laminats = (tame[0])
-tapetes = (tame[1])
-krasa = (tame[2])
+#laminats = (tame[0])
+#tapetes = (tame[1])
+#krasa = (tame[2])
 
 m=len(nauda)
 i=0
@@ -44,9 +45,59 @@ print("Tapetes  pirtkt: ", tame[1], "rullus")
 print("Krāsas pirtkt: ", tame[2], "litru")
 print("Kopā jamāksā: ", round(sum(kopa),2),"Euro")
 
-'''
-2.	Pie jums ir vērsies uzņēmums SIA Stabili podesti, ar vēlmi atvieglot materiālu sagatavošanu finiera podestu ražošanai. Podestus ražo gan standarta izmēros, gan pēc pasūtītāja izmēriem.
-Jūs uzaicina uz ražotni iepazīties ar produkciju, un var novērot, ka podesti principā ir taisnstūra paralēlskaldņi, kas izveidoti no sešām finiera plāksnēm, kas savā starpā sastiprināti ar divpadsmit leņķveida līstēm, un stūros nostiprināti ar astoņiem stūra savienojumiem.
-Uzņēmums vēlas automatizēt izlietoto materiālu uzskaiti un jums tiek uzticēts uzrakstīt kodu funkcijai, kas nosaka materiālu veidu un daudzumu dotā izmēra podestam.
 
+2.	Pie jums ir vērsies uzņēmums SIA Stabili podesti, 
+ar vēlmi atvieglot materiālu sagatavošanu finiera podestu ražošanai. 
+Podestus ražo gan standarta izmēros, gan pēc pasūtītāja izmēriem.
+Jūs uzaicina uz ražotni iepazīties ar produkciju, un var novērot, 
+ka podesti principā ir taisnstūra paralēlskaldņi, 
+kas izveidoti no sešām finiera plāksnēm, 
+kas savā starpā sastiprināti ar divpadsmit leņķveida līstēm, 
+un stūros nostiprināti ar astoņiem stūra savienojumiem.
+Uzņēmums vēlas automatizēt izlietoto materiālu uzskaiti
+un jums tiek uzticēts uzrakstīt kodu funkcijai, 
+kas nosaka materiālu veidu un daudzumu dotā izmēra podestam.
+
+SPECIFIKACIJA:
+Funkcijai “materialuAprekins” ir četri parametri, 
+saskaņā ar šo formātu: materialuAprekins(garums, platums, augstums, skaits). 
+Parametri “garums”, “platums” un “augstums” raksturo podesta izmēru cm, 
+tiem var būt arī viena zīme aiz komata. Parametrs “skaits” ir vesels skaitlis, 
+kas norāda nepieciešamo podestu skaitu.
+Uzskaites programmai informācija tiek nodota ar funkcijas “materialaUzskaite” palīdzību. 
+Šī funkcija vēl nav pagatavota, un to rakstīs citi programmētāji. 
+Šai funkcijai ir četri parametri, saskaņā ar šo formātu: 
+materialaUzskaite(tips, izmers1, izmers2, skaits). 
+Parametrs “tips” ir simbolu virkne, kam atļautas trīs vērtības: 
+“FINIERIS”, “LISTE” un “STURIS”. Ja tips ir “FINIERIS”, 
+tad parametri “izmers1” un” izmers2” apraksta taisnstūrveida finiera plāksnes izmērus, 
+secība nav svarīga. Ja tips ir “LISTE”, tad parametrs “izmers1” 
+apraksta līstes detaļas garumu, savukārt parametra “izmers2” 
+vērtība nav svarīga. Ja tips ir “STURIS”, tad parametru 
+“izmers1” un “izmers2” vērtības nav svarīgas.
+Funkcijas “materialuAprekins” izpildes laikā ir jāizsauc funkcija 
+“materialaUzskaite” ar nepieciešamajiem parametriem katram atšķirīgajam 
+materiālu tipam un izmēram. Ideāli būtu, ja šo izsaukumu skaits būtu pēc iespējas mazāks.
 '''
+
+def materialuAprekins(garums, platums, augstums, skaits):
+
+    return garums*platums*skaits
+
+x=float(input("garums: "))
+y=float(input("platums: "))
+z=float(input("augstums: "))
+r=int(input("skaits: "))
+
+print(materialuAprekins(x,y,z,r))
+
+liste = x
+print("Liste daudzums:", x*12*r)
+sture = r*8
+print("stures daudzums: ", sture)
+finieris = r*x*y
+print("Finiera nepiecešams:",finieris,"cm2")
+
+
+
+
