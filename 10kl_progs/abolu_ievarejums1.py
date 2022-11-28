90b361cd3926eb99f6dc6075612a775e0c26c883
Aleksandr Zelukin <alexandr44@inbox.lv>
2022-11-28 15:03:55 +0200

    Update files

---
+++ 10kl_progs/abolu_ievarejums1.py
@@ -0,0 +1,14 @@
+'''Specifikacija:
+Nepieciešama iespēja definēt recepti un tās sastāvdaļas
+Nepieciešama iespēja definēt, cik maksā katra sastāvdaļa
+Sistēmai jāaprēķina kopējās ievārījuma izmaksas, balstoties uz ievadīto ābolu apjomu
+'''
+
+def ievarijums(aboli_svars, cukurs_uz_kg):
+    cukura_cena = 0.75
+    izmaksa_kg = cukura_cena * cukurs_uz_kg
+    return izmaksa_kg * aboli_svars
+
+aboli = 1.5
+cukurs = 0.7
+print("Uz {} kg ābolu izmaksas būs {} EUR".format(aboli, ievarijums(aboli, cukurs)))
