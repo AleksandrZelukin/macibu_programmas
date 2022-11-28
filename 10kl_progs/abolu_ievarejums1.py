e63358ff01acc0c3f34e39b8e24777d021467bb9
Aleksandr Zelukin <alexandr44@inbox.lv>
2022-11-28 15:11:45 +0200

    Update files

--- 10kl_progs/abolu_ievarejums1.py
+++ 10kl_progs/abolu_ievarejums1.py
@@ -13,5 +13,5 @@ def ievarijums(aboli_svars, cukurs_uz_kg):
 
 aboli = 1.5
 cukurs = 0.7
-#print("Uz {} kg ābolu izmaksas būs {} EUR".format(aboli, ievarijums(aboli, cukurs)))
+print("Uz {} kg ābolu izmaksas būs {} EUR".format(aboli, ievarijums(aboli, cukurs)))
 print("Uz",aboli,"kg ābolu izmaksas būs ",ievarijums(aboli, cukurs))
