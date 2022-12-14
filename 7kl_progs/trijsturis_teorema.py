5730bcdb5bddabb3a70db5e32dd67c69021512a2
Aleksandr Zelukin <alexandr44@inbox.lv>
2022-12-14 11:46:19 +0200

    update programms

--- 7kl_progs/trijsturis_teorema.py
+++ 7kl_progs/trijsturis_teorema.py
@@ -1,7 +1,12 @@
-a = input()
-b = input()
-c = input()
-if a + b > c and a + c > b and b + c > a:
-    print("trijsturis ekziste")
-else:
-    print("trijsturis neekziste")
+while True:
+    h = input("Saksim stadat? (y/n)")
+    if h =="y":
+        a = input("Pirma mala: ")
+        b = input("Otra mala: ")
+        c = input("Treša mala: ")
+        if a + b > c and a + c > b and b + c > a:
+            print("trijsturis ekziste")
+        else:
+            print("trijsturis neekziste")
+    if h =="n":
+        break
