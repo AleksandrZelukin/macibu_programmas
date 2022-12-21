9319792c4f16df84626ad5b516fe34e92499c2b4
Aleksandr Zelukin <alexandr44@inbox.lv>
2022-12-21 10:41:53 +0200

    Create datne

---
+++ 7kl_progs/7a_saraksti_uzd.py
@@ -0,0 +1,14 @@
+#Перевернуть список
+aa = [12,23,34,45,56,67]
+aa.reverse()
+print(aa)
+bb = [45,34,67,12,35,68,37,28]
+dd = bb[::-1]
+print(dd)
+
+
+#Burtu "a" daudzums vardā Latvija
+vards = "Latvija"
+kk = list(vards)
+n = kk.count('a')
+print(n)
