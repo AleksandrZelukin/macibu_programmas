9e09d09a617f7255c73d71d8bd951c8ff63e3b4e
Aleksandr Zelukin <alexandr44@inbox.lv>
2023-01-09 12:35:51 +0200

    Create files

---
+++ 7kl_progs/7.a cikli.py
@@ -0,0 +1,14 @@
+#cikls while
+i = 1
+while i < 6:
+    print(i)
+    i +=1
+#cikls for
+for n in range(1, 6, 1):
+    print(n)
+#cikls while True
+k = 1
+while True:
+    if k < 6:
+        print(k)
+        k +=1
