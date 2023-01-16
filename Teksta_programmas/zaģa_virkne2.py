89aaa0d1b42ba0c37f3ed5812e6aa10a49080330
Aleksandr <alexandr44@inbox.lv>
2023-01-16 21:42:53 +0200

    Update apps

--- Teksta_programmas/zaģa_virkne2.py
+++ Teksta_programmas/zaģa_virkne2.py
@@ -2,8 +2,8 @@
 N = int(input());
 a = list(map(int,input().split()));
 
-for i in range(N):
+for i in a:
     if a[i]>a[i-1] and a[i]>a[1+1]:
-        print(a)
+        print(a[::1])
     #if a[i]<a[i-1] and a[i]<a[i+1]:
     #    print(a)
