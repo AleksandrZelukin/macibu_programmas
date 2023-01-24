87d672977db6e6f1f160140f11bf76fa61eae11c
Aleksandr Zelukin <alexandr44@inbox.lv>
2023-01-24 13:31:59 +0200

    Create apps

---
+++ 7kl_progs/7b_uzdevums.py
@@ -0,0 +1,24 @@
+'''
+1.Programma pieprasa kvadrāta mālu a un izskaitļo perimetru
+
+2.Programma pieprasa Kārļa trīs atzīmes un izskaitļo vidējo aritmētisko.
+
+3.Kārlis nopirka M kg abolu maksājot C centus par kg. 
+Cik centus X ir jāsamaksa par pirkumu? 
+Cik atlikuma centus S viņš dabūs no L eiro?
+
+Annai ir a gadi, bet Marutai - m gadi. Kas no viņam ir vecāka?
+
+Pļavā rotaļājas bērni un suņi. 
+Zinot galvu un kāju kopējo skaitu (g un k), 
+atrast, cik bērnu un cik suņu ir pļavā (b un s).
+
+'''
+nauda = int(input("Man ir: "))
+abolu_cena = float(input("Abulu cena: "))
+pirkums = int(input("Es nopirku: "))
+
+kopa = abolu_cena*pirkums
+atlikums = nauda - kopa
+
+print("Atlikums: ",atlikums)
