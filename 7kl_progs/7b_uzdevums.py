95ca63481cd8ccda228b0493ce596f47e0e135ac
Aleksandr Zelukin <alexandr44@inbox.lv>
2023-01-25 08:46:16 +0200

    Update apps

--- 7kl_progs/7b_uzdevums.py
+++ 7kl_progs/7b_uzdevums.py
@@ -1,7 +1,8 @@
 '''
 1.Programma pieprasa kvadrāta mālu a un izskaitļo perimetru
 
-2.Programma pieprasa Kārļa trīs atzīmes un izskaitļo vidējo aritmētisko.
+2.Programma pieprasa Kārļa trīs atzīmes un izskaitļo 
+vidējo aritmētisko.
 
 3.Kārlis nopirka M kg abolu maksājot C centus par kg. 
 Cik centus X ir jāsamaksa par pirkumu? 
@@ -14,6 +15,19 @@ Zinot galvu un kāju kopējo skaitu (g un k),
 atrast, cik bērnu un cik suņu ir pļavā (b un s).
 
 '''
+atzime1 = int(input())
+atzime2 =int(input())
+atzime3 =int(input())
+videja_atzime= (atzime1+atzime2+atzime3)/3
+print("Vidēja atzime Karļim ir: ", videja_atzime)
+
+side = int(input("Kvadrata māla: "))
+p = side*4
+print("Kvadrata perimetrs: ", p)
+
+
+
+
 nauda = int(input("Man ir: "))
 abolu_cena = float(input("Abulu cena: "))
 pirkums = int(input("Es nopirku: "))
