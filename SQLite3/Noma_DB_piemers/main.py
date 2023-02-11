5f704c430c63661d43b480734544f2f02ec651f5
Aleksandr <alexandr44@inbox.lv>
2023-02-11 22:40:08 +0200

    Update apps

--- SQLite3/Noma_DB_piemers/main.py
+++ SQLite3/Noma_DB_piemers/main.py
@@ -4,30 +4,71 @@ import sqlite3
 app = Flask(__name__)
 
 db = sqlite3.connect('noma.db')
-cur = db.cursor()
+sql = db.cursor()
+sql.execute("""CREATE TABLE IF NOT EXISTS nomnieks(
+    id_nomnieks TEXT PRIMARY KEY,
+    "vards" TEXT, 
+    "uzvards" TEXT, 
+    "talrunis" TEXT
+)""")
 
-cur.execute("""CREATE TABLE IF NOT EXISTS nomnieks (
-    vards TEXT,
-    uzvards TEXT,
-    talrunis TEXT
+sql.execute("""CREATE TABLE IF NOT EXISTS instrumenti (
+    id_instruments TEXT PRIMARY KEY,
+    instruments TEXT,
+    data TEXT,
+    cena TEXT
  )""")
+
+
+sql.execute("""CREATE TABLE IF NOT EXISTS noma (
+    id TEXT PRIMARY KEY,
+    id_nomnieks TEXT NOT NULL,
+    id_instruments TEXT NOT NULL,
+    FOREIGN KEY("id_instruments") REFERENCES "Instrumenti"("id_instruments"),
+    FOREIGN KEY("id_nomnieks") REFERENCES "Nomnieks"("id_nomnieks")
+ )""")
+
+
 db.commit()
 
-
 @app.route('/')
-def index():
-    return render_template("index.html")
+def main():
+    return render_template('index.html')
+
 
 @app.route('/', methods=['POST', 'GET'])
-def nomnieks():
+def nomnieki():
     if request.method == "POST":
+        id_nomnieks = request.form["id_nomnieks"]
         vards = request.form["vards"]
         uzvards = request.form["uzvards"]
         talrunis = request.form["talrunis"]
-        cur.execute('INSERT INTO nomnieks VALUES (?,?,?)')
+        rez = [id_nomnieks,vards,uzvards,talrunis]
+        db = sqlite3.connect('noma.db')
+        sql = db.cursor()
+        sql.execute("INSERT INTO nomnieks VALUES(?,?,?,?)",rez)
+        db.commit()
+        db.close()
+    return render_template('index.html')
 
-db.commit()
-cur.close()
+
+@app.route('/', methods=['POST', 'GET'])
+def instrumenti():
+    if request.method == "POST":
+        id_instruments = request.form["id_instruments"]
+        instruments = request.form["instruments"]
+        data = request.form["data"]
+        cena = request.form["cena"]
+        rez = [id_instruments,instruments,data,cena]
+        db = sqlite3.connect('noma.db')
+        sql = db.cursor()
+        sql.execute("INSERT INTO nomnieks VALUES(?,?,?,?)",rez)
+        db.commit()
+        db.close()
+    return render_template('index.html')
+
+
+
 
 
 if __name__ == "__main__":
