01753c2d6afc1c9398c7b12dba3ff3920b37ec0c
Aleksandr <alexandr44@inbox.lv>
2023-02-11 11:12:29 +0200

    Update apps

--- SQLite3/Noma_DB_piemers/main.py
+++ SQLite3/Noma_DB_piemers/main.py
@@ -1,29 +1,32 @@
 from flask import Flask, render_template, request
 import sqlite3
 
-
 app = Flask(__name__)
 
+con = sqlite3.connect('noma.db')
+cur = con.cursor()
 
-def user_to_db(vards,uzvards,talrunis):
-    con = sqlite3.connect('noma.db')
-    cur = con.cursor()
-    cur.execute('INSERT INTO nomnieks VALUES (?,?,?)')
-    con.commit()
-    con.close()
+cur.execute("""CREATE TABLE IF NOT EXISTS nomnieks (
+    vards TEXT NOT NULL,
+    uzvards TEXT NOT NULL,
+    talrunis TEXT
+ )""")
+con.close()
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
 @app.route('/', methods=['POST', 'GET'])
-if request.method == "POST":
-    vards = request.form["vards"]
-    uzvards = request.form["uzvards"]
-    talrunis = request.form["talrunis"]
-        
-    user_to_db(vards,uzvards,talrunis)
+def nomnieks():
+    if request.method == "POST":
+        vards = request.form["vards"]
+        uzvards = request.form["uzvards"]
+        talrunis = request.form["talrunis"]
+        cur = con.cursor()
+        cur.execute('INSERT INTO nomnieks VALUES (?,?,?)')
 
-
+con.commit()
+cur.close()
 if __name__ == "__main__":
     app.run(debug=True)
