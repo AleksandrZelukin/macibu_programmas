43625c3ae7f553e7e3ce86bf945b0654ec3fbf5d
Aleksandr <alexandr44@inbox.lv>
2023-02-11 10:16:51 +0200

    Update apps

--- SQLite3/Noma_DB_piemers/db.py
+++ SQLite3/Noma_DB_piemers/db.py
@@ -5,27 +5,27 @@ db= sqlite3.connect('noma.db')
 cur = db.cursor()
 
 cur.execute("""CREATE TABLE IF NOT EXISTS nomnieks (
-    id_nomnieks INTEGER PRIMARY KEY AUTOINCREMENT,
+
     vards TEXT NOT NULL,
     uzvards TEXT NOT NULL,
     talrunis TEXT
  )""")
 
-cur.execute("""CREATE TABLE IF NOT EXISTS instrumenti (
-    id_instruments INTEGER PRIMARY KEY AUTOINCREMENT,
-    instruments TEXT NOT NULL,
-    data TEXT NOT NULL,
-    cena INTEGER
- )""")
+# cur.execute("""CREATE TABLE IF NOT EXISTS instrumenti (
+#     id_instruments INTEGER PRIMARY KEY AUTOINCREMENT,
+#     instruments TEXT NOT NULL,
+#     data TEXT NOT NULL,
+#     cena INTEGER
+#  )""")
 
 
-cur.execute("""CREATE TABLE IF NOT EXISTS noma (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    id_nomnieks INTEGER NOT NULL,
-    id_instruments INTEGER NOT NULL,
-    FOREIGN KEY("id_instruments") REFERENCES "Instrumenti"("id_instruments"),
-    FOREIGN KEY("id_nomnieks") REFERENCES "Nomnieks"("id_nomnieks")
- )""")
+# cur.execute("""CREATE TABLE IF NOT EXISTS noma (
+#     id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     id_nomnieks INTEGER NOT NULL,
+#     id_instruments INTEGER NOT NULL,
+#     FOREIGN KEY("id_instruments") REFERENCES "Instrumenti"("id_instruments"),
+#     FOREIGN KEY("id_nomnieks") REFERENCES "Nomnieks"("id_nomnieks")
+#  )""")
 
 # cur.execute("INSERT INTO nomnieks VALUES ('1','Aigars','Ozols','23234556')")
 # cur.execute("INSERT INTO nomnieks VALUES ('2','Olga','Piraga','23234556')")
@@ -45,15 +45,15 @@ cur.execute("""CREATE TABLE IF NOT EXISTS noma (
 #   JOIN nomnieks ON nomnieks.id_nomnieks = noma.id_nomnieks 
 #   JOIN instrumenti ON instrumenti.id_instruments = noma.id_instruments""")
 
-cur.execute("""SELECT vards, uzvards, instruments FROM noma 
-  JOIN nomnieks ON nomnieks.id_nomnieks = noma.id_nomnieks 
-  JOIN instrumenti ON instrumenti.id_instruments = noma.id_instruments""")
+# cur.execute("""SELECT vards, uzvards, instruments FROM noma 
+#   JOIN nomnieks ON nomnieks.id_nomnieks = noma.id_nomnieks 
+#   JOIN instrumenti ON instrumenti.id_instruments = noma.id_instruments""")
 
-# cur.execute("SELECT * FROM nomnieks, noma")
+# # cur.execute("SELECT * FROM nomnieks, noma")
 
-db.commit()
-items = cur.fetchall()
-print(items)
+# db.commit()
+# items = cur.fetchall()
+# print(items)
 
 # for el in items:
 #     print(el[1] + "\n" + el[3])
--- SQLite3/Noma_DB_piemers/main.py
+++ SQLite3/Noma_DB_piemers/main.py
@@ -5,28 +5,24 @@ import sqlite3
 app = Flask(__name__)
 
 
-def user_to_db(id_nomnieks,vards,uzvards,talrunis):
+def user_to_db(vards,uzvards,talrunis):
     con = sqlite3.connect('noma.db')
     cur = con.cursor()
-    cur.execute('INSERT INTO nomnieks VALUES (?,?,?,?)')
+    cur.execute('INSERT INTO nomnieks VALUES (?,?,?)')
     con.commit()
     con.close()
 
-
-
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
 def index():
-    if request.method == "POST":
-        vards = request.form["vards"]
-        uzvards = request.form["uzvards"]
-        talrunis = request.form["talrunis"]
-        rez=(vards,uzvards,talrunis)
-        user_to_db(vards,uzvards,talrunis)
-        
-
     return render_template("index.html")
 
-
+@app.route('/', methods=['POST', 'GET'])
+if request.method == "POST":
+    vards = request.form["vards"]
+    uzvards = request.form["uzvards"]
+    talrunis = request.form["talrunis"]
+        
+    user_to_db(vards,uzvards,talrunis)
 
 
 if __name__ == "__main__":
