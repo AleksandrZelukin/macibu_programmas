8342b87e418a03cdf77f87af3141d61bb993236a
Aleksandr Zelukin <alexandr44@inbox.lv>
2023-02-15 15:13:40 +0200

    Update apps

--- 7kl_progs/7b_dragon_water.py
+++ 7kl_progs/7b_dragon_water.py
@@ -1,19 +1,20 @@
 from random import shuffle
 doors = ['Dragon','Water','Empty']
-life = 3
+life = 2
 count = 0
 while True:
     choise = int(input("Dirvis numurs: "))
-    shuffle(doors)
-    count += 100
-    if doors[choise-1] == 'Dragon':
-        life -=2
-        print('Jūs zaude dzīvibu!')
-    if doors[choise-1] == 'Water':
-        life +=1
-        print('Jūs ieguva dzīvibu!')
-    else:
-        print('Nekas nenotiek!')
-    if life == 0:
-        print('Jūs zaude viss...', count)
-        break
+    if choise < 4 and choise > 0:
+        shuffle(doors)
+        count += 100
+        if doors[choise-1] == 'Dragon':
+            life -=2
+            print('Jūs zaude dzīvibu! un atlikums ', life,"dzīves")
+        if doors[choise-1] == 'Water':
+            life +=1
+            print('Jūs ieguva dzīvibu!un atlikums ', life,"dzīves")
+        if doors[choise-1] == 'Empty':
+            print('Nekas nenotiek!')
+        if life <= 0:
+            print('Jūs zaude viss...', count)
+            break
