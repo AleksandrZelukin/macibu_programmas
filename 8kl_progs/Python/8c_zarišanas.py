1abb654542c029ddde3f6793cb61b60e469adccb
Aleksandr Zelukin <alexandr44@inbox.lv>
2023-02-17 14:25:24 +0200

    Update apps

--- 8kl_progs/Python/8c_zarišanas.py
+++
@@ -1,9 +0,0 @@
-a = int(input("Trijstrura mala a:  "))
-b = int(input("Trijstrura mala b:  "))
-c = int(input("Trijstrura mala c:  "))
-
-if a+b>c and a+c>b and b+c>a:
-    print("Trijssturis ekzistē!")
-
-else:
-    print("Trijssturis NEekzistē!")    
