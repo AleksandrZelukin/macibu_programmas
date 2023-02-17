743746a9daf9c097fcdaaabd55a471042c2aacc0
Aleksandr Zelukin <alexandr44@inbox.lv>
2023-02-17 09:13:00 +0200

    Update apps

--- 7kl_progs/7c_dragon_water.py
+++ 7kl_progs/7c_dragon_water.py
@@ -1,22 +1,38 @@
 from random import shuffle
 durvis = ['Dragon','Water','Empty']
-life = 1
-count = 0
 
-
+rezultats = {}
 while True:
-    izvele = int(input("Durvis numurs- "))
-    shuffle(durvis)
-    count +=100
-    if durvis[izvele-1] == 'Dragon':
-        print('Jūs zaude dzīvibi!')
-        life -=1
-    if durvis[izvele-1] == 'Water':
-        print('Jūs ieguva dzīvibu!')
-        life +=1
-    else:
-        print('Nekas nenotiek!')
+    jautajums = input ('Spelejam? y/n\n')
+    if jautajums == 'y':
+        players = input('Tavs vards:  ')
+        life = 1
+        count = 0
+        while True:
+            izvele = int(input("Durvis numurs- "))
+            if izvele > 3 :
+                print('Durvis ar sadu numuru nekziste')
+                continue
+            shuffle(durvis)
+            count +=100
+            if durvis[izvele-1] == 'Dragon':
+                print('Jūs zaude dzīvibi!')
+                life -=1
+            if durvis[izvele-1] == 'Water':
+                print('Jūs ieguva dzīvibu!')
+                life +=1
+            else:
+                print('Nekas nenotiek!')
 
-    if life == 0:
-        print("Jūs zaude viss...", count)
+            if life == 0:
+                rezultats[players] = count
+                print("Jūs zaude viss...", count)
+                print(rezultats)
+                break
+    if jautajums == 'n':
         break
+
+
+            
+
+            
