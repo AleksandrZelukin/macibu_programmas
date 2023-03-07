20768bb1c60ce798f9f6d9a887c5568878dd1de3
Aleksandr Zelukin <alexandr44@inbox.lv>
2023-03-07 08:24:32 +0200

    update apps

--- 10kl_progs/darbs_ar_failiem.py
+++ 10kl_progs/darbs_ar_failiem.py
@@ -1,19 +1,23 @@
-f = open("darbs_ar_failiem.txt","r") # Skātit fails
-for s in f:
-    print(s, end='')
-f.close()
+# f = open("darbs_ar_failiem.txt","r") # Skātit fails
+# for s in f:
+#     print(s, end='')
+# f.close()
 
-a = input("Ievādi teksts >>")
-f = open("darbs_ar_failiem.txt","w") #Rākstit failā
-print(a, file=f)
-f.close()
+# a = input("Ievādi teksts >>")
+# f = open("darbs_ar_failiem.txt","w") #Rākstit failā
+# print(a, file=f)
+# f.close()
 
-f = open("darbs_ar_failiem.txt","r")
-for s in f:
-    print(s, end='')
-f.close()
+# f = open("darbs_ar_failiem.txt","r")
+# for s in f:
+#     print(s, end='')
+# f.close()
 
-a = input("Ievādi teksts >>")
+# a = input("Ievādi teksts >>")
+a = {}
+vards = input()
+talrunis = input()
+a[vards]=talrunis
 f = open("darbs_ar_failiem.txt","a") #Papīldināt fails
 print(a, file=f)
 f.close()
