dfa662cb01c0407acd219225461a1e14a041e983
Aleksandr Zelukin <alexandr44@inbox.lv>
2023-04-03 15:40:28 +0300

    Update apps

--- 10kl_progs/simpleGUI_widget.py
+++ 10kl_progs/simpleGUI_widget.py
@@ -1,13 +1,14 @@
 import PySimpleGUI as sg
 
 
-layout = [[sg.Text('Sveiki, ka tev sauc?',size=(40))],
-          [sg.InputText()],
+layout = [[sg.Text('Sveiki, ka tev sauc?',size = (16,1)), sg.InputText()],
+          [sg.Text('Kur tu dzivo',size = (15,1)), sg.InputText()],
           [sg.Text('',key='radit', size=(40))],
           [sg.Text('',key='sk', size=(40))],
+          [sg.Multiline(size =(80,6),key = '_multiline_')],
           [sg.Button("Saglabāt datus", key='glab'),
            sg.Button("Skātit dati", key='skat'),
-           sg.Button("Beigt", key='q')]]
+           sg.Button("Beigt", key='q')],]
   
 logs = sg.Window('Mana pirma programma', layout)
 
@@ -16,7 +17,12 @@ while True:
     if event == sg.WINDOW_CLOSED or event == 'q':
       break
     if event == 'skat':
-      #  print(values[0])
-       logs['sk'].update(values[0])
+        rez=[values[0],values[1]]
+        logs['sk'].update(rez)
     if event == 'glab':
        logs['radit'].update('dati saglabāti')
+       f = open("dati.txt","a")
+       rez=[values[0],values[1]]
+       print(rez, file=f)
+       f.close()
+       logs.FindElement('_multiline_').update(rez)
