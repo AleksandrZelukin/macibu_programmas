1d033eb780afa883d3037508f61471fb5fd88d51
Aleksandr Zelukin <alexandr44@inbox.lv>
2023-04-13 15:03:02 +0300

    Update apps

--- 8kl_progs/8a/tkinter_ievads.py
+++ 8kl_progs/8a/tkinter_ievads.py
@@ -2,12 +2,24 @@ from tkinter import *
 win = Tk()
 win.geometry('800x800+700+150')
 
-label1 = Label(text='Hello World!',font=('Arial','24'),bg='green',fg='blue')
-btn1 = Button(text='Press me!',font=('Arial','24'),bg='red',fg='yellow')
-entry1 = Entry(font=('Arial','24'),fg='blue')
+data = StringVar()
 
-label1.pack()
-entry1.pack()
-btn1.pack()
+def izeja():
+    win.destroy()
+
+
+def vards():
+    label2["text"] = entry1.get()
+
+label1 = Label(text='Jusu vārds',font=('Arial','24'),bg='green',fg='blue').pack()
+
+entry1 = Entry(textvariable=data).pack()
+
+btn1 = Button(text='Press me!',font=('Arial','24'),bg='red',fg='yellow',command=vards).pack()
+
+label2 = Label( font=('Arial','24'),fg='blue').pack()
+
+btn2 = Button(text="Izeja",font=('Arial','24'),bg='red',fg='yellow',command=izeja).pack()
+
 
 win.mainloop()
