f912e75c1be8ac47e36e11b80f2ffd95ca6f94b8
Aleksandr <alexandr44@inbox.lv>
2023-04-13 18:35:29 +0300

    Update apps

--- 8kl_progs/8a/tkinter_ievads.py
+++ 8kl_progs/8a/tkinter_ievads.py
@@ -1,28 +1,35 @@
 # fom text.py
 from tkinter import *
 logs = Tk()
-logs.geometry("400x400")
- 
+logs.geometry("500x300+600+150")
+logs.title('Mana pirma programma') 
 
- 
 def get_text():
-    label['text'] = text.get()
+    label2['text'] = text.get()
+
+def remove_text():
+    label2.config(text="")
  
 def iziet():
     logs.destroy()
 
- 
-text = Entry()
-text.grid(row=0,column=0)
+label1=Label(text='uzraksti savu vārdu')
+label1.grid(row=0,column=0)
 
-btn1=Button(text="Paņemiet",command=get_text)
-btn1.grid(row=0,column=1)
+text = Entry()
+text.grid(row=0,column=1)
+
+btn1=Button(text="Paņemt",command=get_text)
+btn1.grid(row=0,column=2)
 
 btn2=Button(text="Beigt",command=iziet)
-btn2.grid(row=1,column=1)
+btn2.grid(row=2,column=1)
 
-label = Label()
-label.grid(row=2,column=0)
+btn3=Button(text="Dzest",command=remove_text)
+btn3.grid(row=1,column=1)
+
+label2= Label()
+label2.grid(row=1,column=0)
 
  
 logs.mainloop()
