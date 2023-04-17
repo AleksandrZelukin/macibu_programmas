a96879051316ecddf7703a088955582b595a39cd
Aleksandr Zelukin <alexandr44@inbox.lv>
2023-04-17 10:41:47 +0300

    Update apps

--- 8kl_progs/8c/Parbaudes_darbs.py
+++ 8kl_progs/8c/Parbaudes_darbs.py
@@ -1,19 +1,21 @@
 from turtle import *
 
-a=int(textinput("Tristruis", "Pamatne"))
-h=int(textinput("Tristruis", "Augstums"))
+a=int(textinput("Trijstrūis", "malas garums"))
+h=int(textinput("Trijstrūis", "augstums"))
 
 for i in range (3):
     fd(150)
     lt(360/3)
-write(a*h,font=("Arial","24"))   
+s = a*h/2    
+write("Trijstūra laukums ir {} ".format(s), font=("Arial","24"))   
 up()
-goto(-150,0)
+goto(-150,-100)
 down()
 r=int(textinput("Aplis", "Radius"))
 
 circle(r)
-write(2*r*3.14,font=("Arial","24"))   
+l = 2*r*3.14
+write("Riņķa līnijas garums ir {} ".format(l),font=("Arial","24"))   
 
 
 
