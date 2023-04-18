6125de518619f4b32dace29f54fa548821e66325
Aleksandr Zelukin <alexandr44@inbox.lv>
2023-04-18 12:02:57 +0300

    Update apps

--- 7kl_progs/7a/tkinter_ievads.py
+++ 7kl_progs/7a/tkinter_ievads.py
@@ -2,14 +2,19 @@ from tkinter import *
 logs = Tk()
 logs.geometry('800x800+600+150')
 
-
+def izeja():
+    print('Programma beidz savu darbu')
+    logs.destroy()
+def teksts():
+    print('Sveiki, draugs!')
+    
 label1 = Label(text='Hello world!',font=('Alrial','24','bold'),bg='blue',fg='yellow')
-entry1 = Entry(font=('Alrial','24','bold'),fg='yellow')
-btn1 = Button(text='Press Me!',font=('Alrial','24','bold'),bg='red',fg='blue')
+entry1 = Entry(font=('Alrial','24','bold'),fg='darkblue')
+btn1 = Button(text='Press Me!',font=('Alrial','24','bold'),bg='green',fg='blue',command=teksts)
+btn2 = Button(text='IZEJA!',font=('Alrial','24','bold'),bg='red',fg='blue',command=izeja)
 
-
-label1.pack()
-entry1.pack()
-btn1.pack()
-
+label1.grid(row=0,column=0)
+entry1.grid(row=0,column=1)
+btn1.grid(row=0,column=2)
+btn2.grid(row=1,column=1)
 logs.mainloop()
