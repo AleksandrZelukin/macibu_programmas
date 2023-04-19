6c476289d79a53fe015a604f00fd6748114e8d3f
Aleksandr Zelukin <alexandr44@inbox.lv>
2023-04-19 10:41:10 +0300

    Update apps

--- 7kl_progs/7a/tkinter_ievads.py
+++ 7kl_progs/7a/tkinter_ievads.py
@@ -7,14 +7,17 @@ def izeja():
     logs.destroy()
 def teksts():
     print('Sveiki, draugs!')
+    label2["text"] = entry1.get()
+    
     
 label1 = Label(text='Hello world!',font=('Alrial','24','bold'),bg='blue',fg='yellow')
 entry1 = Entry(font=('Alrial','24','bold'),fg='darkblue')
 btn1 = Button(text='Press Me!',font=('Alrial','24','bold'),bg='green',fg='blue',command=teksts)
 btn2 = Button(text='IZEJA!',font=('Alrial','24','bold'),bg='red',fg='blue',command=izeja)
-
+label2 = Label(font=('Alrial','24','bold'),fg='blue')
 label1.grid(row=0,column=0)
 entry1.grid(row=0,column=1)
 btn1.grid(row=0,column=2)
 btn2.grid(row=1,column=1)
+label2.grid(row=2,column=1)
 logs.mainloop()
