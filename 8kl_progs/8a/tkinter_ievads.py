e0c026989ee7b9d5d5425be9ea27c5952ba226b4
Aleksandr Zelukin <alexandr44@inbox.lv>
2023-04-19 15:02:38 +0300

    Update apps

--- 8kl_progs/8a/tkinter_ievads.py
+++ 8kl_progs/8a/tkinter_ievads.py
@@ -5,7 +5,10 @@ logs.geometry("600x300+600+150")
 logs.title('Mana pirma programma') 
 
 def get_text():
-    label2['text'] = text.get()
+    label2['text'] = text.get()  
+
+def get_text2():
+    label2['text'] = text2.get(1.0, END)
 
 def remove_text():
     label2.config(text="")
@@ -19,11 +22,14 @@ label1.grid(row=0,column=0)
 text = Entry()
 text.grid(row=0,column=1)
 
-text2 = Text(width=25, height=2)
+text2 = Text(width=20, height=5)
 text2.grid(row=0,column=3)
 
 btn1=Button(text="Paņemt",command=get_text)
 btn1.grid(row=0,column=2)
+
+btn2=Button(text="Paņemt",command=get_text2)
+btn2.grid(row=0,column=4)
 
 btn2=Button(text="Beigt",command=iziet)
 btn2.grid(row=2,column=1)
