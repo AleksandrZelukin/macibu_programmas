9c701a2600a479cc471f503747eb67096a1f6ddb
Aleksandr Zelukin <alexandr44@inbox.lv>
2023-04-20 11:31:19 +0300

    Update apps

--- 8kl_progs/8b/tkinter_ievads.py
+++ 8kl_progs/8b/tkinter_ievads.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 logs = Tk()
 
-logs.geometry('1000x400+800+200')
+logs.geometry('600x400+800+200')
 
 def izeja():
     logs.destroy()
@@ -15,25 +15,37 @@ def izeja():
 def vards():
     label3["text"] = entry1.get()
 
+def vards2():
+    label3["text"] = text1.get(1.0,END)
 
     
-label1 = Label(text='Sveiki!',font=('Arial','24'))
+label1 = Label(text='Sveiki!')
 label1.grid(row=0,column=0)
 
-label2 = Label(text='Kur tu dzivo?',font=('Arial','24'))
+label2 = Label(text='Kur tu dzivo?')
 label2.grid(row=0,column=1)
 
 # entry1 = Entry(textvariable=data,font=('Arial','24'))
-entry1 = Entry(font=('Arial','24'))
+entry1 = Entry()
 entry1.grid(row=0,column=2)
 
-btn1 = Button(text='Saglabāt',command=vards,font=('Arial','24')) 
+btn1 = Button(text='Saglabāt',command=vards) 
 btn1.grid(row=0,column=3)
 
-label3 = Label(font=('Arial','24'))
+text1 = Text(width=20, height=2)
+text1.grid(row=0,column=4)
+
+btn2 = Button(text='Saglabāt',command=vards2) 
+btn2.grid(row=0,column=5)
+r1 = Checkbutton(text='Jā')
+r1.grid(row=1,column=0)
+r2 = Checkbutton(text='Nē')
+r2.grid(row=1,column=1)
+
+label3 = Label()
 label3.grid(row=1,column=1)
 
-btn2 = Button(text='iziet',fg='yellow',bg='red',command=izeja,font=('Arial','24'))
-btn2.grid(row=2,column=1)
+btn2 = Button(text='iziet',fg='yellow',bg='red',command=izeja)
+btn2.grid(row=8,column=3)
 
 logs.mainloop()
