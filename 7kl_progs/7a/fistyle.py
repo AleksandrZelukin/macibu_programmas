14dd0375be23a57378382c8ab7e8999283ff531f
Aleksandr Zelukin <alexandr44@inbox.lv>
2023-05-10 11:33:52 +0300

    update apps

--- 7kl_progs/7a/fistyle.py
+++ 7kl_progs/7a/fistyle.py
@@ -1,5 +1,11 @@
-a = 6
-b = 9
-d = 12
-print(a,"diena")
-print("{},{},{} diena".format(a,b,d))
+# a = 6
+# b = 9
+# d = 12
+# print(a,"diena")
+# print("{},{},{} diena".format(a,b,d))
+
+vards = input("Sveiki! Ka tev sauc?\n")
+
+print('Sveiki,{}, kur tu dzivo?'.format(vards))
+pilseta=input()
+print("labi, {}, tiekamies tur,{}!".format(vards,pilseta)) 
