39232e59f4375a673e521fd17519b27e8dd9885d
Aleksandr Zelukin <alexandr44@inbox.lv>
2023-05-15 09:34:48 +0300

    Update apps

--- 8kl_progs/test.py
+++ 8kl_progs/test.py
@@ -1,7 +1,13 @@
-def get_name_and_decades(name, age):
-    print(f"My name is {name} and I'm {age / 10:.5f} decades old.")
+# def get_name_and_decades(name, age):
+#     print(f"My name is {name} and I'm {age / 10:.5f} decades old.")
 
-get_name_and_decades("Leo", 31)
+# get_name_and_decades("Leo", 31)
+
+
+for i in range(5):
+    if i % 2 ==0:
+        continue
+    print(i)
 
 
 
