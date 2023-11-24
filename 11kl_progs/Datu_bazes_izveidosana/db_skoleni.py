4608c922602c6fe5212a92ade0963566c250c5b3
Aleksandr Zelukin <alexandr44@inbox.lv>
2023-11-24 12:36:34 +0200

    Update apps

--- 11kl_progs/Datu_bazes_izveidosana/db_skoleni.py
+++ 11kl_progs/Datu_bazes_izveidosana/db_skoleni.py
@@ -4,17 +4,30 @@ con = sqlite3.connect("11klase_datu_baze.db")
 cur = con.cursor()
 
 cur.execute("""CREATE TABLE  if not exists dalibnieki(
-            numurs integer,
+            numurs INTEGER PRIMARY KEY,
             vards text,
             uzvards text,
             talrunis text)""")
 cur.execute("""CREATE TABLE if not exists prieksmeti(
-            numurs integer,
-            nosaukums text)""")
+            group_id INTEGER,
+            nosaukums text PRIMARY KEY,
+            FOREIGN KEY (group_id)
+            REFERENCES dalibnieki (group_id) )""")
 
 cur.execute("INSERT INTO dalibnieki VALUES (1, 'Aigars','Ozols' ,'12345678')")
 cur.execute("INSERT INTO dalibnieki VALUES (2, 'Olga','Piraga' ,'87654321')")
 cur.execute("INSERT INTO prieksmeti VALUES (1,'Matematika')")
+cur.execute("INSERT INTO prieksmeti VALUES (2,'Datorika')")
+cur.execute("INSERT INTO prieksmeti VALUES (3,'Sports')")
+cur.execute("INSERT INTO prieksmeti VALUES (4,'Fizika')")
 
-ieraksti = cur.execute("SELECT numurs, vards, uzvards, talrunis FROM dalibnieki").fetchall()
-print(ieraksti)
+dalibnieki = cur.execute("SELECT numurs, vards, uzvards, talrunis FROM dalibnieki").fetchall()
+prieksmeti = cur.execute("SELECT group_id, nosaukums FROM prieksmeti").fetchall()
+
+con.commit()
+print("DALIBNIEKI")
+for i in dalibnieki:
+    print(i)
+print("PRIEKSMETI")
+for m in prieksmeti:
+    print(m)
