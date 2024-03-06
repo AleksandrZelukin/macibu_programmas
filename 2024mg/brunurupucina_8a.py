6f0b7f49dd4be25cf09c84ce6d7fedfe111cc006
Aleksandr Zelukin <alexandr44@inbox.lv>
2024-03-06 12:08:37 +0200

    Update apps

--- 2024mg/brunurupucina_8a.py
+++ 2024mg/brunurupucina_8a.py
@@ -2,11 +2,11 @@ from turtle import *
 import tkinter as tk
 t = Turtle()
 
-krasa = ("red","yellow","blue","lightblue")
+krasa = ("red","yellow","blue","lightblue","black")
 speed(1000)
 def saule():
     pu()
-    goto(-300,200)
+    goto(-300,160)
     pd()
     fillcolor(krasa[1])
     pencolor(krasa[1])
@@ -16,16 +16,9 @@ def saule():
         rt(180)
         rt(10)
     end_fill()
-poga1 = tk.Button(text="SAULE",command=saule)
-poga1.place(x=260,y=180)
-poga2 = tk.Button(text="SIENA")
-poga2.place(x=40,y=70) 
-poga3 = tk.Button(text="DURVIS")
-poga3.place(x=40,y=100)
-poga4 = tk.Button(text="JUMTA")
-poga4.place(x=40,y=130)
-poga5 = tk.Button(text="LOGS")
-poga5.place(x=40,y=160)  
+    pencolor(krasa[4])
+    write("Tas ir SAULE!", align='left', font=('Arial', 18, 'bold'))
+
 pencolor(krasa[0])
 def figura(x,y,z,n,m):
     pu()
@@ -53,11 +46,39 @@ def truba(x,y,z,m):
     fd(z)
     lt(90)
     end_fill()
-        
-siena = figura(-75,-380,150,4,0)
-durvis = truba(-60,-380,80,3)
-logs = figura(10,-300,40,4,3)
-caurule = truba(20,-122,60,0)
-jumta = figura(-100,-230,200,3,1)
+          
+def siena ():
+    figura(-75,-380,150,4,0)
+def durvis():
+    truba(-60,-380,80,3)
+def logs():
+    figura(10,-300,40,4,3)
+def caurule ():
+    truba(20,-122,60,0)
+def jumta():
+    truba(20,-122,60,0)
+    figura(-100,-230,200,3,1)
+    
+
+poga1 = tk.Button(text="SAULE",command=saule)
+#poga1.place(x=260,y=140)
+poga1.place(relx=.4,rely=.2)
+poga2 = tk.Button(text="SIENA",command=siena)
+#poga2.place(x=40,y=70)
+poga2.place(relx=.5,rely=.2)
+poga3 = tk.Button(text="DURVIS",command=durvis)
+#poga3.place(x=40,y=100)
+poga3.place(relx=.6,rely=.2)
+poga4 = tk.Button(text="JUMTA",command=jumta)
+#poga4.place(x=40,y=130)
+poga4.place(relx=.7,rely=.2)
+poga5 = tk.Button(text="LOGS", command=logs)
+#poga5.place(x=40,y=160)
+poga5.place(relx=.8,rely=.2)  
+
 
 mainloop()
+
+
+#pencolor(krasa[4])
+#write("Tas ir DŪMENIS!", align='left', font=('Arial', 18, 'bold'))  
