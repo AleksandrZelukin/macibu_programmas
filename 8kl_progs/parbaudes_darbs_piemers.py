a7f56b1be583c1071ca23f584637a7d7f0675d41
Aleksandr Zelukin <alexandr44@inbox.lv>
2024-03-18 10:01:30 +0200

    Update apps

--- 8kl_progs/parbaudes_darbs_piemers.py
+++ 8kl_progs/parbaudes_darbs_piemers.py
@@ -4,11 +4,26 @@ import turtle as tu
 logs=tu.Screen()
 
 def kvadrat():
+    tu.clear()
     for i in range(4):
         tu.fd(100)
         tu.lt(90)
+        
+def trijsturis():
+    tu.clear()
+    for i in range(3):
+        tu.fd(100)
+        tu.lt(120)
+        
+def tirit():
+    tu.clear()
 
 btn=tk.Button(text='kvadrats',command=kvadrat)
 btn.place(x=100,y=100)
+btn2=tk.Button(text='trijsturis',command=trijsturis)
+btn2.place(x=100,y=150)
 
+
+btn_c=tk.Button(text='Dzest',command=tirit)
+btn_c.place(x=100,y=250)
 tu.mainloop()
