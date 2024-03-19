0cf4868f8dee2d6db06a99c68c3e2528e652a1d6
Aleksandr Zelukin <alexandr44@inbox.lv>
2024-03-19 12:29:00 +0200

    Update apps

--- 8kl_progs/parbaudes_darbs_piemers.py
+++ 8kl_progs/parbaudes_darbs_piemers.py
@@ -4,55 +4,43 @@ import turtle as tu
 logs=tu.Screen()
 
 def kvadrat():
-    vietax = tu.textinput("Jautājums","Norādi X")
-    vietay = tu.textinput("Jautājums","Norādi Y")
+    x = int(tu.textinput("","Norādi X"))
+    y = int(tu.textinput("","Norādi Y"))
+    z = int(tu.textinput("","Kvadrāta malas garums"))
     tu.up()
-    tu.goto(int(vietax),int(vietay))
+    tu.goto(x,y)
     tu.down()
     for i in range(4):
-        tu.fd(100)
+        tu.fd(z)
         tu.lt(90)
-    tu.write("Kvadrats")
+    tu.write(f"Kvadrats ar malas garumu {z}px")
     tu.up()
-    tu.home()   
+    tu.home()
+    
 def trijsturis():
-    vietax = tu.textinput("Jautājums","Norādi X")
-    vietay = tu.textinput("Jautājums","Norādi Y")
-    tu.goto(int(vietax),int(vietay))
+    x = int(tu.textinput("Jautājums","Norādi X"))
+    y = int(tu.textinput("Jautājums","Norādi Y"))
     tu.up()
-    tu.goto(int(vietax),int(vietay))
+    tu.goto(x,y)
     tu.down()
     for i in range(3):
         tu.fd(100)
         tu.lt(120)
     tu.write("Trijsturis")
     tu.up()
-    tu.home()
+    tu.home()   
+    
+def dzesana():
+    tu.clear()   
 
-def zvaigznite():
-    vietax = tu.textinput("Jautājums","Norādi X")
-    vietay = tu.textinput("Jautājums","Norādi Y")
-    tu.goto(int(vietax),int(vietay))
-    tu.up()
-    tu.goto(int(vietax),int(vietay))
-    tu.down()
-    tu.lt(36)
-    for i in range(5):
-        tu.fd(100)
-        tu.lt(144)
-    tu.write("Zvaigznite")
-    tu.up()
-    tu.home()      
-def tirit():
-    tu.clear()
 
 btn=tk.Button(text='kvadrats',command=kvadrat)
 btn.place(x=100,y=100)
 btn2=tk.Button(text='trijsturis',command=trijsturis)
 btn2.place(x=100,y=150)
-btn3=tk.Button(text='zvaigznite',command=zvaigznite)
+btn3=tk.Button(text='zvaigznite')
 btn3.place(x=100,y=200)
 
-btn_c=tk.Button(text='Dzest',command=tirit)
+btn_c=tk.Button(text='Dzest',command=dzesana)
 btn_c.place(x=100,y=250)
 tu.mainloop()
