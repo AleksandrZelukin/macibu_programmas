fc991f77c2a4b587568ddd19e089c2b8da2c4aca
Aleksandr Zelukin <alexandr44@inbox.lv>
2024-04-05 13:10:20 +0300

    Update apps

--- 7kl_progs/akmens-skeres-papirs_7c.py
+++ 7kl_progs/akmens-skeres-papirs_7c.py
@@ -1,26 +1,25 @@
-from random import choice
+import random
 saraksts = ["akmens","šķēres","papīrs"]
-dators = choice(saraksts)
-speletajs = input("akmens/šķēres/papīrs:")
+#random.shuffle(saraksts)
+#print(saraksts[0])
+dators = random.choice(saraksts)
+speletajs = input("Jūsu izvēlē\n(akmens,šķēres,papīrs): ")
 
 if dators == speletajs:
     print("neizšķirts")
-    print(f"Dators āri izvēlējās {dators}")
-if (dators == "akmens") and (speletajs == "šķēres"):
-    print(f"Dators izvēlējās {dators}")
+if dators == "akmens" and speletajs == "šķēres":
     print("dators uzvareja")
-if (dators == "šķēres") and (speletajs == "papīrs"):
+if dators == "šķēres" and speletajs == "papīrs":
     print("dators uzvareja")
-    print(f"Dators izvēlējās {dators}")
-if (dators == "papīrs") and (speletajs == "akmens"):
+if dators == "papīrs" and speletajs == "akmens":
     print("dators uzvareja")
-    print(f"Dators izvēlējās {dators}")
-if (dators == "akmens") and (speletajs == "papīrs"):
+    
+if speletajs == "akmens" and dators == "šķēres":
     print("Tu uzvareji")
     print(f"Dators izvēlējās {dators}")
-if (dators == "šķēres") and (speletajs == "akmens"):
+if speletajs == "šķēres" and dators == "papīrs":
     print("Tu uzvareji")
     print(f"Dators izvēlējās {dators}")
-if (dators == "papīrs") and (speletajs == "šķēres"):
+if speletajs == "papīrs" and dators == "akmens":
     print("Tu uzvareji")
     print(f"Dators izvēlējās {dators}")
