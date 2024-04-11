e9fd7e8ba2c1c20f5929692eb31d3fe924beeccb
Aleksandr Zelukin <alexandr44@inbox.lv>
2024-04-11 08:48:44 +0300

    Update apps

--- 8kl_progs/Python/autorizacija_8c_v2.py
+++ 8kl_progs/Python/autorizacija_8c_v2.py
@@ -3,25 +3,25 @@ win = tk.Tk()
 win.title("Ieeja sistēma")
 win.geometry("300x400")
 a = {"Ivars":"123","Olga":"123","Igors":"789"}
-
 res = tk.Label(win, text = " ")
 res.place(relx=.2, rely=.4)
 nos = tk.Label(win, text = "Ievadiet Vārds u parole")
 nos.place(relx=.2, rely=.0)
 
-
-def reizinajums():
-    vards = str(vards.get())
-    parole = str(parole.get())
-    if a.get(vards) != parole:
-        res.configure(text="Lietotājvārds vai parole nesakrīt!")
-    else:
-        res.configure("OK!")
-        
 vards = tk.Entry(win)
 vards.place(relx=.2, rely=.1)
 parole = tk.Entry(win)
 parole.place(relx=.2, rely=.2)
+
+def reizinajums():
+    v = vards.get()
+    p = parole.get()
+    if a.get(v) != p:
+        res.configure(text="Lietotājvārds vai parole nesakrīt!")
+    else:
+        res.configure(text="OK!")
+        
+
    
 button1 = tk.Button(win, text = "Ieeja",
                     background="#FF00AA",
