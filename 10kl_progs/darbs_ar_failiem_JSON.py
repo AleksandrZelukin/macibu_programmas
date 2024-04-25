7cd29fd205106c2624ee46d1e26078ce712dbd96
Aleksandr Zelukin <alexandr44@inbox.lv>
2024-04-25 11:10:07 +0300

    Update apps

--- 10kl_progs/darbs_ar_failiem_JSON.py
+++ 10kl_progs/darbs_ar_failiem_JSON.py
@@ -2,12 +2,12 @@ import json
 
 aa = [11,22,33,44,55]
 #bb = {'tris':(3,'tris')}
-f = open("10kl_progs/darbs_ar_faliem.txt","w")
+f = open("darbs_ar_faliem_JSON.txt","a")
 json.dump((aa),f)
 f.close()
 
 
-f = open("10kl_progs/darbs_ar_failiem.txt", "r")
+f = open("darbs_ar_failiem_JSON.txt", "r")
 a = json.load(f)
 
 print(a)
