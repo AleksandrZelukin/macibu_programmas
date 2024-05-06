8ca6ad89d85d757ac07ad20c165f0f5675221763
Aleksandr <alexandr44@inbox.lv>
2024-05-06 20:28:50 +0300

    Update apps

--- 7kl_progs/izsuvesana_paraugs.py
+++ 7kl_progs/izsuvesana_paraugs.py
@@ -1,11 +1,24 @@
 from turtle import *
-shape("square")
-up()
-stamp()
-fd(20)
-stamp()
-lt(90)
-for i in range(10):
-    fd(25)
-    stamp()
+pensize(4)
+def krustins():
+    lt(45)
+    fd(50)
+    up()
+    lt(135)
+    fd(35)
+    lt(135)
+    down()
+    fd(50)
+    lt(45)
+x,y = 0, 0
+for i in range(5):
+    y += 40
+    x = 0
+    for j in range(5):
+        up()
+        goto(x,y)
+        down()
+        x += -40 
+        krustins()
+
 done()
