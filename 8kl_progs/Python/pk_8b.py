dd99a834b21ef7100fe63bf02381d33f89cef392
Aleksandr Zelukin <alexandr44@inbox.lv>
2024-05-07 12:31:25 +0300

    update apps

--- 8kl_progs/Python/pk_8b.py
+++ 8kl_progs/Python/pk_8b.py
@@ -1,18 +1,22 @@
 from datetime import datetime
 pk = input("Personas kods: ")
-'''
-if len(pk) == 12:
-    print("pk OK!")
-else:
-    print("not OK!") 
-'''
-pk1 = (''.join(list(pk[0:6])))
-'''
-dd = pk1[0:2]
-mm = pk1[2:4]
-yy = pk1[4:6]
-print(dd,mm,yy)
-'''
 
-datumu_parbaude = datetime.strptime(pk1, "%d%m%y")
-print(datumu_parbaude)
+if len(pk) == 12:
+    print("pk garums OK!")
+else:
+    print("not OK!")
+
+pk1 = list(pk)
+milenium = pk1[7]
+if milenium == "1":
+    pk1.insert(4,"19")
+if milenium == "2":
+    pk1.insert(4,"20")
+    
+pk1 = pk1[0:7]
+pk1 = (''.join(pk1))
+try:
+    datumu_parbaude = datetime.strptime(pk1, "%d%m%Y")
+    print(datumu_parbaude)
+except:
+    print("Personas kods nepareizs!")
--- 8kl_progs/Python/pk_8c.py
+++ 8kl_progs/Python/pk_8c.py
@@ -1,21 +1,19 @@
 from datetime import datetime
 pk = input("Ievadi personas kods: ")
 if len(pk) == 12:
-    print("Personas koda garums pareizs!")
     pk1 = list(pk)
-    dd = pk1[0:2]
-    mm = pk1[2:4]
-    gg = pk1[4:6]    
     if pk1[7] == '1':
-        gg.insert(0,"19")
+        pk1.insert(4,"19")
     elif pk1[7] == '2':
-        gg.insert(0,"20")
+        pk1.insert(4,"20")
     else:
         print("Personas koda dati nav pareizi!")
-    dzimsanas_date = (''.join(dd),''.join(mm),''.join(gg))
-    date_kontrole=datetime.strptime((''.join(dzimsanas_date)), "%d%m%Y")
-    print(date_kontrole)
+    pk1=pk1[0:7]
+    try:
+        date_kontrole=datetime.strptime((''.join(pk1)), "%d%m%Y")
+        print(date_kontrole)
+    except:
+        print("Personas kods neekzistē")
 else:
     print("Personas koda garums nav pareizs!")
-    
-
+  
