eb353a339b138327dcdbc3262101c656f99b72fb
Aleksandr Zelukin <alexandr44@inbox.lv>
2024-05-09 11:23:10 +0300

    update apps

--- 7kl_progs/ornamets_7a.py
+++ 7kl_progs/ornamets_7a.py
@@ -2,15 +2,18 @@ from turtle import *
 from random import randint
 speed(400)
 bgcolor("pink")
-pensize(1)
+pensize(4)
 colormode(255)
-
-for i in range(36):
-    r = (randint(0,255))
-    g = (randint(0,255))
-    b = (randint(0,255))
-    color(r,g,b)
-    circle(100,360,8)
-    lt(10)
-
+def rinkis(x,y):
+    up()
+    goto(x,y)
+    down()
+    for i in range(6):
+        r = (randint(0,255))
+        g = (randint(0,255))
+        b = (randint(0,255))
+        color(r,g,b)
+        circle(50)
+        lt(60)
+onscreenclick(rinkis)
 done()
