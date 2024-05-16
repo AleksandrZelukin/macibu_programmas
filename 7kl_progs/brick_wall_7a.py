21ea711efebb11c6483261eb01ad2c4eef3efa80
Aleksandr Zelukin <alexandr44@inbox.lv>
2024-05-16 08:38:13 +0300

    Update apps

--- 7kl_progs/brick_wall_7a.py
+++ 7kl_progs/brick_wall_7a.py
@@ -1,25 +1,29 @@
 from turtle import *
+from random import randint
 speed(1000)
+krasa = ["red","blue","brown","orange","pink","green","grey"]
+numurs = randint(0,6)
+
 def kegelis (x,y):
     for m in range (24):
+        num = randint(0,6)
         y += 30
         if m% 2 == 0:
-            x = -425
+            x = -425            
         else:
-            x = -450
-            
+            x = -450           
         for k in range(16):
             up()
             setposition(x,y)
-            down()   
+            down() 
+            color(krasa[num])
+            begin_fill()  
             for i in range(2):
                 fd(50)
                 rt(90)
                 fd(25)
                 rt(90)
-            x += 55    
-    
-            
-        
+            x += 55  
+            end_fill()          
 kegelis(-450,-370)
 done()
