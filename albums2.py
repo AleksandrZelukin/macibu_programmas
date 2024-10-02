5b567d56297ab7b2f6318d05c0f9272ccfc4a1cd
Aleksandr Zelukin <alexandr44@inbox.lv>
2024-10-02 14:57:37 +0300

    Update apps

--- albums2.py
+++ albums2.py
@@ -1,39 +1,45 @@
 import sqlite3
 
-conn = sqlite3.connect("albums2.db")
+conn = sqlite3.connect("albums3.db")
+conn.execute("PRAGMA foreign_keys = 1")
 cur = conn.cursor()
 
 cur.execute("""CREATE TABLE if not exists band (
             band_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            nosaukums TEXT)""")
+            band_nosaukums TEXT)""")
 
 cur.execute("""CREATE TABLE if not exists albums (
             albums_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            nosaukums TEXT,
-            band_id,
+            album_nosaukums TEXT,
+            band_id INTEGER,
             FOREIGN KEY (band_id) REFERENCES band(band_id))""")
+'''
+cur.execute("INSERT INTO band (band_nosaukums) VALUES('The Beatles')")
+cur.execute("INSERT INTO band (band_nosaukums) VALUES('Pink Floyd')")
+cur.execute("INSERT INTO band (band_nosaukums) VALUES('Deep Purple')")
 
-cur.execute("INSERT INTO band (nosaukums) VALUES('The Beatles')")
-cur.execute("INSERT INTO band (nosaukums) VALUES('Pink Floyd')")
-cur.execute("INSERT INTO band (nosaukums) VALUES('Deep Purple')")
-
-cur.execute("INSERT INTO albums (nosaukums, band_id) VALUES('Sgt. Peppers Lonely Hearts Club Band',1)")
-cur.execute("INSERT INTO albums (nosaukums, band_id) VALUES('Yellow Submarine',1)")
-cur.execute("INSERT INTO albums (nosaukums, band_id) VALUES('Revolver',1)")
-cur.execute("INSERT INTO albums (nosaukums, band_id) VALUES('The Wall',2)")
-
-
-cur.execute('SELECT * FROM band')
+cur.execute("INSERT INTO albums (album_nosaukums, band_id) VALUES('Sgt. Peppers Lonely Hearts Club Band',1)")
+cur.execute("INSERT INTO albums (album_nosaukums, band_id) VALUES('Yellow Submarine',1)")
+cur.execute("INSERT INTO albums (album_nosaukums, band_id) VALUES('Revolver',1)")
+cur.execute("INSERT INTO albums (album_nosaukums, band_id) VALUES('The Wall',2)")
+'''
+cur.execute('SELECT band_nosaukums FROM band')
 all_band = cur.fetchall()
 print("Gruppas")
 for band in all_band:
   print(band)
 
-cur.execute('SELECT * FROM albums')
+cur.execute('SELECT album_nosaukums FROM albums')
 all_albums = cur.fetchall()
 print("Albums")
 for albums in all_albums:
   print(albums)
-cur.execute("SELECT * FROM band JOIN albums ON albums.band_id = band.band_id ")
+  
+print("visi kopa")
+cur.execute("SELECT * FROM band JOIN albums ON band.band_id = albums.band_id ")
+kopa = cur.fetchall()
+for visi_kopa in kopa:
+  print(visi_kopa)
+
 conn.commit()
 cur.close()
