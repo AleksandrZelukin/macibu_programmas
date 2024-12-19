7e60eb270f2a5a6bc18898aa6d2d6b94c824e435
Aleksandr Zelukin <alexandr44@inbox.lv>
2024-12-19 13:49:28 +0200

    Update apps

---
+++ 11kl_progs/Kolatsa_hipoteze.py
@@ -0,0 +1,13 @@
+#Гипотеза Коллатца
+a = int(input())
+count=0
+while a>1:
+    if a%2==0:
+        a = a//2
+        print(a)
+
+    elif a%2!=0:
+        a = 3*a+1
+        print(a)
+    count += 1    
+    print(f"{count} soļos")
