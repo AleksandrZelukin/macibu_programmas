b650320ae543ee39bba223c77ea286de493f6300
Aleksandr Zelukin <alexandr44@inbox.lv>
2025-01-20 11:51:15 +0200

    Update apps

--- Speles/akmens_papirs_skeres.py
+++ Speles/akmens_papirs_skeres.py
@@ -1,8 +1,23 @@
 import random
 
-# print(random.randint(1,3))
-# m = ("a","b","c")
-# f=random.choices(m)
-# print(f)
+print("""
+      ==========
+      Akmens - 1
+      Šķēres - 2
+      Papīrs - 3
+      ==========
+      """)
 
-print("1 - akmens")
+vardi = {1:"Akmens",2:"Šķēres",3:"Papīrs"}
+
+speletajs = int(input("Jūsu izvēlē: "))
+dators = random.randint(1,3)
+print("Jūsu izvēļe: ",vardi[speletajs])
+print("Datora izvēlē:",vardi[dators])
+
+if speletajs == dators:
+    print("spēles izloze")
+elif speletajs == 1 and dators == 2 or speletajs == 2 and dators == 3 or speletajs == 3 and dators == 1:
+    print("Jūs uzvāreja!") 
+else:
+    print("Dators uzvāreja!")
