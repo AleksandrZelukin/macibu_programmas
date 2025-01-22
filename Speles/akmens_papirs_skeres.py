fe263caa88c480f874df9542964fd3b3cd6c4896
Aleksandr Zelukin <alexandr44@inbox.lv>
2025-01-22 15:35:51 +0200

    Update apps

--- Speles/akmens_papirs_skeres.py
+++ Speles/akmens_papirs_skeres.py
@@ -1,5 +1,4 @@
 import random
-
 print("""
       ==========
       Akmens - 1
@@ -8,16 +7,30 @@ print("""
       ==========
       """)
 
-vardi = {1:"Akmens",2:"Šķēres",3:"Papīrs"}
+vardi = {1:"Akmens",2:"Šķēres",3:"Papīrs",4:"Stop, Spēles beigas!"}
+counter = 0
+spele = 1
+while True:
+    print("Spēles numurs: ",spele)
+    speletajs = int(input("Jūsu izvēlē: "))
+    dators = random.randint(1,3)
+    print("Jūsu izvēļe: ",vardi[speletajs])
+    print("Datora izvēlē:",vardi[dators])
+ 
+    if speletajs == dators:
+        print("spēles izloze") 
+        spele += 1  
+    if speletajs == 1 and dators == 2 or speletajs == 2 and dators == 3 or speletajs == 3 and dators == 1:
+        counter += 1
+        spele += 1
+        print("Jūs uzvāreja!, kopējais konts: ",counter) 
+    if speletajs == 4:
+        break
+    else:
+        spele += 1
+        counter -= 1
+        print("Dators uzvāreja, kopējais konts: ",counter)
+        
 
-speletajs = int(input("Jūsu izvēlē: "))
-dators = random.randint(1,3)
-print("Jūsu izvēļe: ",vardi[speletajs])
-print("Datora izvēlē:",vardi[dators])
-
-if speletajs == dators:
-    print("spēles izloze")
-elif speletajs == 1 and dators == 2 or speletajs == 2 and dators == 3 or speletajs == 3 and dators == 1:
-    print("Jūs uzvāreja!") 
-else:
-    print("Dators uzvāreja!")
+    
+    
