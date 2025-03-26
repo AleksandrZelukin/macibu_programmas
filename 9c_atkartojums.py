ad012019360c1a2aebaa0c70fb798ccb2ae4fc33
Aleksandr Zelukin <alexandr44@inbox.lv>
2025-03-26 14:12:09 +0200

    Create apps

---
+++ 9c_atkartojums.py
@@ -0,0 +1,20 @@
+from math import pi
+a = "Riga"
+b = str(12)
+print(a+b)
+
+
+a = 12
+b = 17
+s = a + b
+s = a - b
+s = a * b
+s = a / b
+s = a // b
+s = a % b
+s = a ** b   
+print(s)
+
+r = 125
+s = pi*r**2
+print(s)
