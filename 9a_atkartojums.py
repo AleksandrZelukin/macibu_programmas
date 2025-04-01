fb7a1f353910b4f00093c0b3789e2b912b6aa795
Aleksandr Zelukin <alexandr44@inbox.lv>
2025-04-01 13:57:50 +0300

    Update apps

--- 9a_atkartojums.py
+++ 9a_atkartojums.py
@@ -1,9 +1,14 @@
-import math
-from math import sqrt, pi
-a = 3
-b = 4
-c = sqrt(a**2 + b**2)
-r = 45
-s = pi*r**2
-print(s)
-print(math.dir())
+# import math
+# from math import sqrt, pi
+# a = 3
+# b = 4
+# c = sqrt(a**2 + b**2)
+# r = 45
+# s = pi*r**2
+# print(s)
+# print(math.dir())
+
+a = 7
+b = (3, 4, 5)
+c = [1, 2, 3]
+d = {1: "a", 2: "b"}
