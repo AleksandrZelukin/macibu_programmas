215001c995c40a47f92a01a32f06436fa9e55f36
Aleksandr Zelukin <alexandr44@inbox.lv>
2025-04-22 09:18:36 +0300

    Update apps

--- 9b_atkartojums.py
+++ 9b_atkartojums.py
@@ -1,3 +1,19 @@
-a = (1,4,12,45,78,90,100)
-
-
+a = "Grāmata"
+# print(a)
+a1 = list(a)
+# print(a1)
+# a2 = a1[::-1]
+# print(a2)
+# a3 = " ".join(a2)
+# print(a3)
+# a4 = a3.upper()
+# print(a4)
+lat = ["ā","Ā","ē","Ē","ī","Ī","ķ","Ķ","ļ","Ļ","ņ","Ņ","š","Š","ū","Ū","ž","Ž"]
+d = 0
+for i in a1:
+    if i in lat:
+        d += 1
+        print(f"Vārdā {a} ir {d} burtus ar diakritiskām zīmēm")
+    else:
+        print(f"Vārdā {a} nav diakritisko zīmju")
+        break
